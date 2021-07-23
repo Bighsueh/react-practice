36fb6986df51d7458c574958718ad1fdcbba1ebc
Bighsueh <40841121@gm.nfu.edu.tw>
2021-07-24 03:51:36 +0800

    add SafeAreaView from react-native-paper

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,TouchableHighlight,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import {ProgressBar} from "react-native-paper";
-
 
 export default function App() {
     console.log("console_log");
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       {/*ProgressBar*/}
       <Text>ProgressBar</Text>
       <ProgressBar progress={0.3} style={styles.probarStyle} color={'#febc5f'}/>
 
-    </View>
+    </SafeAreaView>
   );
 }
 
