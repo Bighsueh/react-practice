cc7c54232ecce5d898064567b7772d3fda917e49
Bighsueh <40841121@gm.nfu.edu.tw>
2021-07-24 02:25:06 +0800

    add ProgressBar from react-native-paper

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
-
+import { StyleSheet, Text, View,TouchableHighlight,TouchableOpacity } from 'react-native';
+import {ProgressBar} from "react-native-paper";
 
 
 export default function App() {
@@ -9,6 +9,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      {/*ProgressBar*/}
+      <Text>ProgressBar</Text>
+      <ProgressBar progress={0.3} style={styles.probarStyle} color={'#febc5f'}/>
 
     </View>
   );
