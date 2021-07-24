a9d582cc744cd95a445d16df02b59892f0f7ebca
Bighsueh <40841121@gm.nfu.edu.tw>
2021-07-25 03:05:29 +0800

    add logo,text and two bars

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Image, ImageBackground, StyleSheet, View} from "react-native";
-import createCSSStyleSheet from "react-native-web/dist/exports/StyleSheet/createCSSStyleSheet";
+import {Image, ImageBackground, StyleSheet, Text, View} from "react-native";
+import {normalize} from "react-native-elements";
 
 function WelcomeScreen(props) {
     return (
@@ -8,8 +8,13 @@ function WelcomeScreen(props) {
             source={require('../assets/hehuan_mountain.jpg')}
             style={styles.background}
         >
-            {/*<View style={styles.loginButton}/>*/}
+            <View style={styles.logoContainer}>
+                <Image source={require('../assets/turtle.png')} style={styles.logo}/>
+                <Text style={styles.logoFont}> 因為，山就在那裏 </Text>
+            </View>
 
+            <View style={styles.loginButton}/>
+            <View style={styles.registerButton}/>
         </ImageBackground>
     );
 }
@@ -19,11 +24,31 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
     background: {
         flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
     },
-    // loginButton: {
-    //     width: '100%',
-    //     height: 70,
-    //     backgroundColor : 'aqua',
-    //
-    // },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: 'cadetblue',
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: 'darkgray',
+    },
+    logo: {
+        width: 100,
+        height: 100,
+    },
+    logoContainer: {
+        position: "absolute",
+        top: 70,
+        alignItems: "center",
+    },
+    logoFont: {
+        color: "white",
+        fontSize: normalize(36)
+    },
+
 })
