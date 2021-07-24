e1ebc814cc9e7ffb908e312e131b6578103ed05d
Bighsueh <40841121@gm.nfu.edu.tw>
2021-07-24 12:53:09 +0800

    add TouchableOpacity from react-native-paper

--- App.js
+++ App.js
@@ -1,33 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {StyleSheet, Text, View, Image, SafeAreaView,TouchableOpacity} from 'react-native';
 import {ProgressBar} from "react-native-paper";
 
 export default function App() {
-    console.log("console_log");
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      {/*ProgressBar*/}
-      <Text>ProgressBar</Text>
-      <ProgressBar progress={0.3} style={styles.probarStyle} color={'#febc5f'}/>
+    console.log("storing change...");
+    const handlePress = () => console.log("text pressed");
+    const handleImgPress = ()=> console.log('img pressed')
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto"/>
+            {/*ProgressBar*/}
+            <Text>ProgressBar</Text>
+            <ProgressBar progress={0.3} style={styles.probarStyle} color={'#febc5f'}/>
+            <Text numberOfLines={1} onPress={handlePress}>
+                山川壯麗，物產豐隆，炎黃世胄，東亞稱雄。
+                毋自暴自棄，毋故步自封，光我民族，促進大同。
+                創業維艱，緬懷諸先烈，守成不易，莫徒務近功。
+                同心同德，貫徹始終，青天白日滿地紅。
+            </Text>
+            <TouchableOpacity onPress={handleImgPress}>
+                <Image source={require("./assets/Taiwan.jpg")} style={styles.img}/>
+            </TouchableOpacity>
 
-    </SafeAreaView>
-  );
+        </SafeAreaView>
+    );
 }
 
 
 // 樣式
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  probarStyle: {
-    width: 162,
-    height: 8,
-    backgroundColor: "#E0E0E0"
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    probarStyle: {
+        width: 162,
+        height: 8,
+        backgroundColor: "#E0E0E0"
+    },
+    img: {
+        resizeMode: 'center',
+    },
 });
